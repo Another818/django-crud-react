16e8f0f3ba5084e373cbf06817e8bd8fee1655d1
Tomás Menna <pablotomasmenna@gmail.com>
2023-09-22 01:59:40 -0300

    Arreglo de configuracion Vercel

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-je#a5m57m^=zttbw&v*$!lt9h=uvr0-jznm6!m!b^9-2-lo(8!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
