59a31e437d49711d7a45d41a8192474479663a9a
Tomás Menna <pablotomasmenna@gmail.com>
2023-09-23 03:01:31 -0300

    installed front

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,8 +1,15 @@
 import axios from 'axios';
 
+const URL =
+    process.env.NODE_ENV === "production"
+        ? import.meta.env.VITE_BACKEND_URL
+        : "http://localhost:8000";
+
+console.log(URL);
 const taskApi = axios.create({
-    baseURL: 'http://localhost:8000/tasks/api/v1/tasks/'
+    baseURL: `${URL}/tasks/api/v1/tasks`,
 });
+
 
 export const getAllTasks = () => taskApi.get('/');
 
