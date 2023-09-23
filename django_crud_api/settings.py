9d3b3aa898ddb8a87d039ea1ec2f428778679267
Tomás Menna <pablotomasmenna@gmail.com>
2023-09-23 00:25:33 -0300

    Arreglando deploy a Railway5

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-je#a5m57m^=zttbw&v*$!lt9h=uvr0-jznm6!m!b^9-2-lo(8!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['localhost','127.0.0.1', 'django-crud-react-production-f420.up.railway.app']
 
 
 # Application definition
